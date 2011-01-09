091a6e9cbd03f28dda22a6d6e9505b6f511e54cf
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2011-01-08 19:07:25 -0600

    Adding the stub for the line editor project.

---
+++ src/MfGames.GtkExt.LineTextEditor/Interfaces/ILineBuffer.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MfGames.GtkExt.LineTextEditor
+{
+	/// <summary>
+	/// Defines the interface for a collection of lines used for text editing.
+	/// </summary>
+	public interface ILineBuffer
+	{
+	}
+}
+
