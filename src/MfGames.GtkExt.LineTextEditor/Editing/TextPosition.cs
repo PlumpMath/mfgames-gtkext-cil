b708eb6d95c0aa74a8632313fc83e1cddc08ba58
D. Moonfire <d.moonfire@mfgames.com>
2011-01-14 19:16:45 -0600

    Added a text position structure to use as coordinates into the text buffer.

---
+++ src/MfGames.GtkExt.LineTextEditor/Editing/TextPosition.cs
@@ -0,0 +1,67 @@
+#region Copyright and License
+
+// Copyright (c) 2009-2011, Moonfire Games
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#endregion
+
+namespace MfGames.GtkExt.LineTextEditor.Editing
+{
+	/// <summary>
+	/// Represents a position within the text buffer using the line as a primary
+	/// and the character within the line's text.
+	/// </summary>
+	public struct TextPosition
+	{
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TextPosition"/> struct.
+		/// </summary>
+		/// <param name="line">The line.</param>
+		/// <param name="character">The character.</param>
+		public TextPosition(
+			int line,
+			int character)
+			: this()
+		{
+			Line = line;
+			Character = character;
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the character.
+		/// </summary>
+		/// <value>The character.</value>
+		public int Character { get; set; }
+
+		/// <summary>
+		/// Gets or sets the line.
+		/// </summary>
+		/// <value>The line.</value>
+		public int Line { get; set; }
+
+		#endregion
+	}
+}
