43ef3c07aac2e96eb380a03657e1c85f41753b70
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2011-09-27 23:44:35 -0500

    Merging code.

--- src/MfGames.GtkExt.Actions/Layouts/LayoutAction.cs
+++ src/MfGames.GtkExt.Actions/Layouts/LayoutAction.cs
@@ -1,6 +1,6 @@
 #region Copyright and License
 
-// Copyright (c) 2009-2011, Moonfire Games
+// Copyright (c) 2005-2011, Moonfire Games
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -35,70 +35,72 @@ using MfGames.Reporting;
 
 namespace MfGames.GtkExt.Actions.Layouts
 {
-	/// <summary>
-	/// Represents a single item in the layout.
-	/// </summary>
-	public class LayoutAction : ILayoutListItem
-	{
-		#region Constructors
+    /// <summary>
+    /// Represents a single item in the layout.
+    /// </summary>
+    public class LayoutAction : ILayoutListItem
+    {
+        #region Constructors
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="LayoutAction"/> class.
-		/// </summary>
-		/// <param name="reader">The reader.</param>
-		public LayoutAction(XmlReader reader)
-		{
-			ActionName = reader["name"];
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LayoutAction"/> class.
+        /// </summary>
+        /// <param name="reader">The reader.</param>
+        public LayoutAction(XmlReader reader)
+        {
+            ActionName = reader["name"];
+        }
 
-		#endregion
+        #endregion
 
-		#region Properties
+        #region Properties
 
-		/// <summary>
-		/// Gets or sets the name.
-		/// </summary>
-		/// <value>The name.</value>
-		public string ActionName { get; set; }
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string ActionName { get; set; }
 
-		#endregion
+        #endregion
 
-		#region Population
+        #region Population
 
-		/// <summary>
-		/// Populates the specified shell with sub-menus.
-		/// </summary>
-		/// <param name="manager"></param>
-		/// <param name="shell">The shell.</param>
-		public void Populate(
-			ActionManager manager,
-			MenuShell shell)
-		{
-			// Get the action associated with this.
-			Action action = manager.GetAction(ActionName);
-			MenuItem menuItem;
+        /// <summary>
+        /// Populates the specified shell with sub-menus.
+        /// </summary>
+        /// <param name="manager"></param>
+        /// <param name="shell">The shell.</param>
+        public void Populate(
+            ActionManager manager,
+            MenuShell shell)
+        {
+            // Get the action associated with this.
+            Action action = manager.GetAction(ActionName);
+            MenuItem menuItem;
 
-			if (action == null)
-			{
-				// Create a placeholder menu item.
-				menuItem = new MenuItem("<Unknown Action: " + ActionName + ">");
-				menuItem.Sensitive = false;
+            if (action == null)
+            {
+                // Create a placeholder menu item.
+                menuItem = new MenuItem("<Unknown Action: " + ActionName + ">");
+                menuItem.Sensitive = false;
 
-				// Add it to the errors.
-				manager.Messages.Add(
-					new SeverityMessage(
-						Severity.Error, "Could not find action " + ActionName + " to add to menu."));
-			}
-			else
-			{
-				// Create a menu item from this action.
-				menuItem = new ActionMenuItem(manager, action);
-			}
+                // Add it to the errors.
+                manager.Messages.Add(
+                    new SeverityMessage(
+                        Severity.Error,
+                        "Could not find action " + ActionName +
+                        " to add to menu."));
+            }
+            else
+            {
+                // Create a menu item from this action.
+                menuItem = new ActionMenuItem(manager, action);
+            }
 
-			// Add the resulting menu item to the list.
-			shell.Add(menuItem);
-		}
+            // Add the resulting menu item to the list.
+            shell.Add(menuItem);
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
